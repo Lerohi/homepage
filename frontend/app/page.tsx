9033e0d2408bc1e3355b6ccee1fced0936ec654f
Lerohi <lerohi.real@gmail.com>
2025-12-27 03:10:33 +0100

    Pipeline verification

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1>Hello from Next.js 🚀</h1>
+          <h1>Second hello from Next.js 🚀</h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
